2f75070e69cc2f05c082dbab56af5baa737eed7e
farmamark <farmamark@icloud.com>
2024-05-16 15:31:59 +0200

    feat (Internationalization)

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module TestGuru
     #
     config.time_zone = "Warsaw"
     config.active_record.default_timezone = :local
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :en
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
